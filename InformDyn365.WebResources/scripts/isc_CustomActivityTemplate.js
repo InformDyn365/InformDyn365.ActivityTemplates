6833a61338d124463bcc95fc47b81b76776e8239
Chris.H <Chris@Harringtonmail.com>
2017-07-06 15:12:46 -0400

    Initial Commit

---
+++ InformDyn365.WebResources/scripts/isc_CustomActivityTemplate.js
@@ -0,0 +1,22 @@
+﻿var FORM_TYPE_CREATE = 1;
+var FORM_TYPE_UPDATE = 2;
+var FORM_TYPE_READ_ONLY = 3;
+var FORM_TYPE_DISABLED = 4;
+var FORM_TYPE_QUICK_CREATE = 5;
+var FORM_TYPE_BULK_EDIT = 6;
+
+function formOnLoad() {
+
+    var formType = Xrm.Page.ui.getFormType();
+
+    if (formType == FORM_TYPE_CREATE) {
+        Xrm.Page.getControl('WebResource_ShowEntityFields').setVisible(false);
+        Xrm.Page.getAttribute('isc_customentityname').setRequiredLevel('none');
+    }
+    if (formType == FORM_TYPE_UPDATE) {
+        Xrm.Page.getControl('WebResource_ShowEntityFields').setVisible(true);
+        Xrm.Page.getAttribute('isc_customentityname').setRequiredLevel('required');
+        
+    }
+
+};
